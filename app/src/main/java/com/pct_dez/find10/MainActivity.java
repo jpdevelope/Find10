69d10bf1f8cb67f76f7d0d131ba55419e15508c9
João Paulo Lima <45554090+jpdevelope@users.noreply.github.com>
2020-04-28 06:34:38 -0300

    Alguns ajustes nos ícones

--- app/src/main/java/com/pct_dez/find10/MainActivity.java
+++ app/src/main/java/com/pct_dez/find10/MainActivity.java
@@ -19,10 +19,25 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
-    String mTitle[] = {"Facebook","Whatsapp","Twitter","Instagran","Youtube"};
-    String mDescription[] = {"Facebook Description","Whatsapp Description","Twitter Description","Instagran Description",
-            "Youtube Description"};
-    int images[] = {R.drawable.facebook,R.drawable.whatsapp,R.drawable.twitter,R.drawable.instagram,R.drawable.youtube};
+    String mTitle[] = {"Adonai Móveis & Eletros", "Afins Cosméticos", "Clube da Ortodontia - Dr. Gutemberg",
+            "Comercial Vieira - Org. Sr. Vieira & filhos", "Farmácia dos Remédios Popular", "Kim Climatização",
+            "Papelaria & Livraria Mais Cultura", "Pastelaria Caldo de Cana", "W & E Imóveis - Soluções Imbiliárias",
+            "3R Informática - Suporte Técnico Especializado", "Casa da Merenda - Sr. Dedé & Família", "Castro - Costurando Ideias"};
+    String mDescription[] = {"Av. Lúcio José de Menezes, 1340 - Croatá I - Pacajus-Ce.",
+            "Rua Luiz Cláudio, 196A - Centro - Pacajus-Ce.",
+            "Rua Poeta Pedro Cesário, 360, altos, sala 01, Centro - Pacajus-Ce.",
+            "Rua Alipio Oliveira Lima, 90 (Mercado dos Cereais - Box 39) - Centro - Pacajus-Ce.",
+            "Rua Engenheiro João Bosco, 218, Centro. Entregamos em domicílio: 3348-0900 / 99202-3536",
+            "Rua Antônio Germano de Sousa, 575, Buriti - Pacajus-Ce.",
+            "Rua Cônego Eduardo Araripe, 1646 - Centro - Pacajus-Ce.",
+            "Av. Lúcio José de Menezes, 1336 - Croatá I, Pacajus-Ce.",
+            "Rua Naíde Costa de Menezes, 59, Centro - Pacajus-Ce.",
+            "Av. Lúcio José de Menezes, 1001, Croatá I - Pacajus-Ce.",
+            "Rua Poeta Pedro Cesário, 361, Centro - Pacajus-Ce.",
+            "Org. Ivanir Castro - (85) 9 9178-2548"};
+    int images[] = {R.drawable.adonailogo, R.drawable.afins_logo, R.drawable.clube_orto_logomarca, R.drawable.comercial_vieira_logo,
+            R.drawable.farmacia_popular_logomarca,R.drawable.kim_logomarca,R.drawable.mais_cultura_logo,R.drawable.pastelaria_logomarca,
+    R.drawable.we_logomarca,R.drawable.roney,R.drawable.casa_merenda_logo,R.drawable.ivanirlogo};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,34 +52,56 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position ==  0) {
-                    Toast.makeText(MainActivity.this, "Facebook Description", Toast.LENGTH_SHORT).show();
+                if (position == 0) {
+                    Toast.makeText(MainActivity.this, "Adonai Móveis & Eletros", Toast.LENGTH_SHORT).show();
                 }
-                if (position ==  0) {
-                    Toast.makeText(MainActivity.this, "Whatsapp Description", Toast.LENGTH_SHORT).show();
+                if (position == 1) {
+                    Toast.makeText(MainActivity.this,"Afins Cosméticos", Toast.LENGTH_SHORT).show();
                 }
-                if (position ==  0) {
-                    Toast.makeText(MainActivity.this, "Twitter Description", Toast.LENGTH_SHORT).show();
+                if (position == 2) {
+                    Toast.makeText(MainActivity.this, "Clube da Ortodontia - Dr. Gutemberg", Toast.LENGTH_SHORT).show();
                 }
-                if (position ==  0) {
-                    Toast.makeText(MainActivity.this, "Instagram Description", Toast.LENGTH_SHORT).show();
+                if (position == 3) {
+                    Toast.makeText(MainActivity.this, "Comercial Vieira - Org. Sr. Vieira & filhos", Toast.LENGTH_SHORT).show();
                 }
-                if (position ==  0) {
-                    Toast.makeText(MainActivity.this, "Youtube Description", Toast.LENGTH_SHORT).show();
+                if (position == 4) {
+                    Toast.makeText(MainActivity.this, "Farmácia dos Remédios Popular", Toast.LENGTH_SHORT).show();
                 }
+                if (position == 5) {
+                    Toast.makeText(MainActivity.this, "Kim Climatização", Toast.LENGTH_SHORT).show();
+                }
+                if (position == 6) {
+                    Toast.makeText(MainActivity.this, "Papelaria & Livraria Mais Cultura", Toast.LENGTH_SHORT).show();
+                }
+                if (position == 7) {
+                    Toast.makeText(MainActivity.this, "Pastelaria Caldo de Cana", Toast.LENGTH_SHORT).show();
+                }
+                if (position == 8) {
+                    Toast.makeText(MainActivity.this, "W & E Imóveis - Soluções Imobiliárias", Toast.LENGTH_SHORT).show();
+                }
+                if (position == 9) {
+                    Toast.makeText(MainActivity.this, "3R Informática - Suporte Técnico Especializado", Toast.LENGTH_SHORT).show();
+                }
+                if (position == 10) {
+                    Toast.makeText(MainActivity.this, "Casa da Merenda - Sr. Dedé & Família", Toast.LENGTH_SHORT).show();
+                }
+                if (position == 11) {
+                    Toast.makeText(MainActivity.this, "Castro - Costurando Ideias", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
     }
 
-    class MyAdapter extends ArrayAdapter<String>{
+    class MyAdapter extends ArrayAdapter<String> {
 
         Context context;
         String rTitle[];
         String rDescription[];
         int rImgs[];
 
-        MyAdapter (Context c, String[] title, String[] description, int[] imgs){
+        MyAdapter(Context c, String[] title, String[] description, int[] imgs) {
             super(c, R.layout.row, R.id.textView1, title);
             this.context = c;
             this.rTitle = title;
